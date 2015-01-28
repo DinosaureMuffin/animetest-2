5198936d2725cb33c6d804ede2acb057937cf100
Damien MOULIN <damien.moulin1@gmail.com>
2015-01-28 11:07:13 +0100

    Delete accueil.php

--- accueil.php
+++
@@ -1,7 +0,0 @@
-	<h1>new title !</h1>
-	<p>
-	Damien courant d'air frais qui n'en ai neni grace a l'automne</p>
-	<img src="images/Chaton-roux.jpg" width="160" height="90"><br/>
-	<img src="images/Fd.jpg" width="192" height="120"><br/>
-	<p>Noel pd</p>
-	<p>Damien gay</p>
